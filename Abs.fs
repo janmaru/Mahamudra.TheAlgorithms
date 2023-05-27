ecd7629d24f9db9757acc8f248bea75cef1f39a7
janmaru <janmaru@gmail.com>
2023-05-28 01:58:52 +0200

    Aggiungere i file di progetto.

---
+++ Abs.fs
@@ -0,0 +1,17 @@
+﻿namespace Mahamudra.TheAlgorithms
+
+module Abs =
+    let abs x = 
+        match x with
+        | x when x < 0 ->  -x 
+        | _ -> x
+
+    let abs' x = 
+        match x with
+        | x when x < 0. ->  -x 
+        | _ -> x
+
+    let abs'' x = 
+        match x with
+        | x when x < 0M ->  -x 
+        | _ -> x 
